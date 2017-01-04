46b47d2f6aaa0b06440a0dd2ebe1d9228796ad62
CoderMessi <2546712148@qq.com>
2017-01-04 15:48:33 +0800

    first commit

---
+++ SwordClient/SwordClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwordClient
+//
+//  Created by 宋瑞航 on 2017/1/4.
+//  Copyright © 2017年 SRH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SwordClient/SwordClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwordClient
+//
+//  Created by 宋瑞航 on 2017/1/4.
+//  Copyright © 2017年 SRH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
